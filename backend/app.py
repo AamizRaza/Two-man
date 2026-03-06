3391f84ce8c034962e552c42a9f2b36669b1b17e
AamizRaza <6858024056@student.chula.ac.th>
2026-03-06 12:56:06 +0700

    Fix import statement for models in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from database import SessionLocal, engine
-from . import models
+import models
 
 # create tables if they don't exist
 models.Base.metadata.create_all(bind=engine)
